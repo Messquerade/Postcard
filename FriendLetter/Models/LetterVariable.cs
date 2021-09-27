e127a1e1115e6e8020466707827dcb109cb1eee0
Messquerade <annac.klingberg@gmail.com>
2021-09-27 09:30:46 -0700

    add gitignore

---
+++ FriendLetter/Models/LetterVariable.cs
@@ -0,0 +1,15 @@
+namespace FriendLetter.Models
+{
+  public class LetterVariable
+  {
+    public string Recipient { get; set; }
+    public string Sender { get; set; }
+    public string Location { get; set; }
+
+    public string Description { get; set; }
+
+    public string AlternateVacation { get; set; }
+
+    public string Souvenir { get; set; }
+  }
+}
